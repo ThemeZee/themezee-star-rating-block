0228d6aab81091db182be01bd598bb777033cd68
Thomas Weichselbaumer <kontakt@themecoder.de>
2022-11-18 15:00:34 +0100

    added German translation

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'bc947f98305bef04a62e');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '6a45d7052cb89963b4e5');
